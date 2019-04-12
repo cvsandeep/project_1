85238caf06737abff2df1d5343099e73a41cd857
Venkata <sandeep.venkata@hp.com>
2019-04-12 15:03:33 -0700

    Added duty cycle detection through software

--- software/src/prj_main.c
+++ software/src/prj_main.c
@@ -9,6 +9,7 @@
 #include "functional_interface.h"
 
 uint64_t 	timestamp = 0L;
+volatile u8 duty_cycle[3];
 
 /************************** MAIN PROGRAM ************************************/
 int main(void)
@@ -17,7 +18,6 @@ int main(void)
 
 	uint32_t sts;
 	u8 hue, sat, val;
-	u8 r_duty = 12, g_duty = 34, b_duty = 56;
 	bool detect;
 
 
@@ -75,7 +75,7 @@ int main(void)
 		}
 	}
 	xil_printf("Starting Main Application\n");
-	laststate = ENC_getState(&pmodENC_inst);
+
 	OLEDrgb_SetFontColor(&pmodOLEDrgb_inst ,OLEDrgb_BuildHSV(255,255,255));  // blue font
 	while ( IsExit() )
 	{
@@ -103,8 +103,7 @@ int main(void)
 			//UpdateSWDutyCycle();
 		}
 
-		DisplayDutycycle(r_duty, g_duty, b_duty);
-		laststate = state;
+		DisplayDutycycle(duty_cycle[0], duty_cycle[1], duty_cycle[2]);
 		//usleep(1000);
 	}
 
@@ -141,8 +140,45 @@ int main(void)
 * Project 1 this could be a reasonable place to do that processing.
  *****************************************************************************/
 
+#define rising_detect 1
+#define falling_detect 2
+#define duty_calc 3
+
+volatile bool signal[3];
+volatile bool old_signal[3];
+volatile u8 type = 0;
+volatile u8 high_level[3];
+volatile u8 low_level[3];
+
 void FIT_Handler(void)
 {
 	// Read the GPIO port to read back the generated PWM signal for RGB led's
 	gpio_in = XGpio_DiscreteRead(&GPIOInst0, GPIO_0_INPUT_0_CHANNEL);
+
+	// Take value PWM for each color Red, Green Blue and then shift this bit to significant bit
+	signal[0] = (gpio_in & 0x04) >> 2;
+	signal[1] = (gpio_in & 0x01) >> 0;
+	signal[2] = (gpio_in & 0x02) >> 1;
+
+	for (u8 color=0; color < 3; color++)
+	{
+		if(!old_signal[color] && signal[color]) //detect_rising_edge(signal);
+		{
+			duty_cycle[color] = (high_level[color]*100)/(high_level[color]+low_level[color]);
+			high_level[color] = 1;
+		}
+		else if(old_signal[color] && !signal[color]) //detect_failing_edge(signal);
+		{
+			low_level[color] = 0;
+		}
+		else if(old_signal[color] && signal[color])
+		{
+			high_level[color]++;
+		}
+		else if(!old_signal[color] && !signal[color])
+		{
+			low_level[color]++;
+		}
+
+	}
 }
